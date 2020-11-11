d5efc7b543eb9692b0b6535be543cc830ea1578a
Christian Freitas <62871678+ChristianSF@users.noreply.github.com>
2020-11-11 19:31:03 -0300

    Add files via upload

---
+++ Java/contas.java
@@ -0,0 +1,17 @@
+public class contas {
+    public static void main(String args[]) {
+       int a = 30;
+       int b = 10;
+       int c = 10;
+       int total = (a+b+c)/10;
+        System.out.println("O resultado eh = " +total);
+        
+        if(total < 5 ) {
+            System.out.println("É menor que 5");
+        }
+        
+        else {
+            System.out.println("Não é menor que 5");
+        }
+    }
+ }
---
+++ Java/teste.java
@@ -0,0 +1,12 @@
+public class teste {
+    
+    private String nome;
+    private String endereco;
+    private int telefone;
+    
+    int[] notas = new int[4];
+    
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
